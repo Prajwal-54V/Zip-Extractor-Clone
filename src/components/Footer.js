23d5930fd8d976853e58bbf5c47b11bece58a901
prajwal-54 <prajjumoger54@gmail.com>
2022-09-10 14:11:37 +0530

    header css updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,32 @@ import "../css/Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="copyright" id="copyright">
-        ©&nbsp;123apps Computer Software Est. 2012–2022
-      </div>
-      <div className="menu">
-        <a href="/legal">Privacy</a> <a href="/pricing">Pricing</a>
-        <button
-          className="feedback_button"
-          data-desc="Please use this form to get in touch with us, report a bug, or suggest a feature."
-          href="#"
+    <div className="container-fluid footer">
+      <div className="row">
+        <div className="col-6 d-md-flex flex-row footer-left ms-4 p-3">
+          <div className="box copyright me-3" id="copyright">
+            ©&nbsp;123apps Computer Software Est. 2012-2022
+          </div>
+          <div className="box menu">
+            <a href="/#">Privacy</a> <a href="/#">Pricing</a>
+            <button
+              className="feedback_button"
+              data-desc="Please use this form to get in touch with us, report a bug, or suggest a feature."
+              href="/#"
+            >
+              Contact Us
+            </button>
+          </div>
+        </div>
+        <a
+          className="col d-flex flex-row justify-content-end  align-items-center p-1 me-3"
+          id="language-link"
+          href="/#"
         >
-          Contact Us
-        </button>
+          <i className="icon-language"></i>
+          <span id="lang">English</span>
+        </a>
       </div>
-      <a
-        id="language-link"
-        href="/#"
-        data-toggle="modal"
-        data-dismiss="modal"
-        data-target="#modal-language"
-      >
-        <i className="icon icon-language"></i>
-        <span>English</span>
-      </a>
     </div>
   );
 }
